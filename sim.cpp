a71e449b752db940d8000ec86ee2ccc3062a4bbb
Daniel Andraško <48550611+sinep-TheShifu@users.noreply.github.com>
2021-12-06 05:03:21 +0100

    Add files via upload

---
+++ sim.cpp
@@ -0,0 +1,47 @@
+#include <iostream> 
+#include <queue>    // priority_queue
+#include <cstdlib>  // exit()
+#include "simlib.h"
+
+using namespace std;
+
+// Error message and quit program
+void error(const char *msg)
+{
+    cerr << "ERROR: " << msg << endl;
+    exit(1);
+}
+
+// System input model 
+class Generator : public Event 
+{
+    void Behaviour()
+    {
+        (new Customer)->Activate();
+        Activate(Time+Exponential(1));  // TODO 
+    }
+}
+
+Facility Box("Box");
+Store 
+
+// Time of model
+double Time;
+
+int main()
+{
+
+    cout << "*** Inicializacia" << endl;
+
+    Time = 0;
+
+    cout << "Time = " << Time << endl;
+
+    schedule(event1, Time);     // Prva aktivacia
+
+    while (!calendar.empty())
+    {
+        
+    }
+    
+}
