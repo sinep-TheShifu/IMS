97612bbbb4244bfae3c90c0d15805a1cced3fd14
sinep-theshifu <xandra05@vutbr.cz>
2021-12-12 21:41:54 +0100

    Uprava pojmov, vytvorenie dokumentacie

--- sim.cpp
+++ sim.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cstdlib>  // exit()
+#include <cstdlib>  
 #include <cstring>
 #include "simlib.h"
 
@@ -19,16 +19,23 @@ Facility myciBox("MyciBox");
 Store zamestnanecNaCisteni("zamestanecNaCisteni", pocet_zamestancov_cistenie);
 Store zamestnanecNaDesign("zamestnanecNaDesign", pocet_zamestancov_dizajn);
 Store zamestnanecNaLepeni("zamestanecNaLepeni", pocet_zamestancov_lepenie);
-Store miestaPreAuta("miestaPreAuta", pocet_parkovacich_miest);
-Store autoNaPozicanie("autoNaPozicanie", pocet_pozicovanych_aut);
-
-// Histogram
-Histogram dokonceniePoziadavku("Celkova doba poziadavku v systeme", 0, 60, 20);
-Histogram pozicanieAuta("...", 0, 60, 15);
+Store miestaPrevozidla("miestaPrevozidla", pocet_parkovacich_miest);
+Store vozidloNaPozicanie("vozidloNaPozicanie", pocet_pozicovanych_aut);
 
 void help()
 {
-    cout << "Help" << endl;
+    cout << "usage: ./sim [<options> ...] \n\n"
+    "<options> is one of\n"
+    "-pzc <count>\n"
+    "\tnumber of employees working in cleaning\n"
+    "-pzd <count>\n"
+    "\tnumber of employees working on design\n"
+    "-pzl <count>\n"
+    "\tnumber of employees working on glueing\n"
+    "-ppm <count>\n"
+    "\tnumber of parking spots inside the building\n"
+    "-ppa <count>\n"
+    "\tnumber of rental vehicles\n" << endl;
     exit(0);
 }
 
@@ -37,7 +44,7 @@ void spracovanie_argumentov(int argc, char * argv[])
     cout << "*** Spracovanie argumentov" << endl;
     for (int i = 1; i < argc ; i++)
     {
-        if ((strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) && argv[i + 1] != NULL)
+        if ((strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0))
         {
             help();
         }            
@@ -116,7 +123,8 @@ void spracovanie_argumentov(int argc, char * argv[])
     cout << endl;
 }
 
-class PozicanieAuta : public Process 
+// Proces pozicania vozidla
+class Pozicanievozidla : public Process 
 {
     void Behavior()
     {
@@ -125,66 +133,64 @@ class PozicanieAuta : public Process
 
         if (pravdepodobnost <= 15)
         {
-            // Je volne auto na pozicanie
-            if (autoNaPozicanie.Free() != 0)
+            // Je volne vozidlo na pozicanie
+            if (vozidloNaPozicanie.Free() != 0)
             {
-                Enter(autoNaPozicanie, 1);
-                cout << "Pozicanie auta: Zakaznik si pozical 1 auto." << endl;    
+                Enter(vozidloNaPozicanie, 1);
+                cout << "Pozicanie vozidla: Zakaznik si pozical 1 vozidlo." << endl;    
 
                 // Najkratsi mozny proces je cistenie vozidla : 2h + 6h = 8h
                 // Najdlhzsi proces je lepenie designom : 2h + 12h + 25h = 39h
                 // + Exp(4h) je priblizna doba vozidla stravena vo frontach
-                Wait(Exponential(480) + Uniform(0, 2340) + Exponential(240));
+                Wait(Uniform(480, 2580));
 
-                Leave(autoNaPozicanie, 1);
-                cout << "Pozicanie auta: Zakaznik vratil pozicane auto." << endl;
+                Leave(vozidloNaPozicanie, 1);
+                cout << "Pozicanie vozidla: Zakaznik vratil pozicane vozidlo." << endl;
             }
-            // Nieje volne auto na pozicanie
+            // Nieje volne vozidlo na pozicanie
             else
             {
-                cout << "Pozicanie auta: Zakaznik si chcel pozicat auto, ale nebolo dostupne." << endl;
+                cout << "Pozicanie vozidla: Zakaznik si chcel pozicat vozidlo, ale nebolo dostupne." << endl;
                 return;
             }
         } 
-        // Zakaznik si auto nepozicia s 85% pravdepodobnostou
+        // Zakaznik si vozidlo nepozicia s 85% pravdepodobnostou
         else
         {
-            cout << "Pozicanie auta: Zakaznik si auto nechce pozicat." << endl;
+            cout << "Pozicanie vozidla: Zakaznik si vozidlo nechce pozicat." << endl;
             return;
         }       
     }
 };
 
-// Zakaznik
+// Proces zakaznik
 class Zakaznik : public Process 
 {
     void Behavior()
     {
-        //double prichod = Time;
-
-        // Volne parkovacie miesta pre auta v dielni
-        if(miestaPreAuta.Free() != 0)
+        // Volne parkovacie miesta pre vozidla v dielni
+        if(miestaPrevozidla.Free() != 0)
         {
 
             // GOTO pre zakaznika ktory sa druhy krat vracia a je volne miesto
             obsluha: 
             int pravdepodobnost = Uniform(0, 100);
 
-            Enter(miestaPreAuta, 1);
-            cout << "Miesta pre auta: Zaberam 1 parkovacie miesto na dielni." << endl;
+            Enter(miestaPrevozidla, 1);
+            cout << "Miesta pre vozidla: Zaberam 1 parkovacie miesto na dielni." << endl;
 
-            // Vytvara sa proces pozicania auta  
-            (new PozicanieAuta)->Activate();
+            // Vytvara sa proces pozicania vozidla  
+            (new Pozicanievozidla)->Activate();
                 Activate(Time);
 
             // Cistenie
             if (pravdepodobnost <= 20) // 20% pravdepodobnost
             {    
                 Enter(zamestnanecNaCisteni, 1);
-                cout << "Cistenie info: Zaberam 1 zamestnanca na cistenie auta" << endl;
+                cout << "Cistenie info: Zaberam 1 zamestnanca na cistenie vozidla" << endl;
 
                 Seize(myciBox);
-                cout << "Cistenie info: Zaberam mici box na cistenie auta" << endl;
+                cout << "Cistenie info: Zaberam mici box na cistenie vozidla" << endl;
 
                 Wait(Exponential(120));
 
@@ -192,13 +198,13 @@ class Zakaznik : public Process
                 cout << "Cistenie info: Uvolnujem myci box" << endl;
 
                 Wait(Exponential(360));
-                cout << "Cistenie info: Auto je vycistene a pripravene na predaj zakaznikovy" << endl;
+                cout << "Cistenie info: vozidlo je vycistene a pripravene na predaj zakaznikovy" << endl;
 
                 Leave(zamestnanecNaCisteni, 1);
                 cout << "Cistenie info: Uvolnujem 1 zamestnanca na cistenie" << endl;
 
-                Leave(miestaPreAuta, 1);
-                cout << "Miesta pre auta: Uvolnujem 1 parkovacie miesto na dielni." << endl;
+                Leave(miestaPrevozidla, 1);
+                cout << "Miesta pre vozidla: Uvolnujem 1 parkovacie miesto na dielni." << endl;
             }
             // Polep dizajnom
             else  if(pravdepodobnost > 20 && pravdepodobnost <= 40 ) // 20% pravdepodobnost
@@ -207,9 +213,9 @@ class Zakaznik : public Process
                 cout << "Dizajn info: Zabiram 1 zamestance na design" << endl;
 
                 Seize(myciBox);
-                cout << "Dizajn info: Zabiram myci box pred designem auta" << endl;
+                cout << "Dizajn info: Zabiram myci box pred designem vozidla" << endl;
 
-                Wait(Exponential(120)); // myti auta
+                Wait(Exponential(120)); // myti vozidla
 
                 Release(myciBox);
                 cout << "Dizajn info: Uvolnuji myci box a jdu na tvorbu designu" << endl;
@@ -220,19 +226,19 @@ class Zakaznik : public Process
                 cout << "Dizajn info: Uvolnuji 1 zamestance na design a jdu na tisk designu" << endl;
 
                 Wait(360); // tisk designu
-                cout << "Dizajn info: Tisk dokoncen, auto je pripravene na nalepeni designu" << endl;
+                cout << "Dizajn info: Tisk dokoncen, vozidlo je pripravene na nalepeni designu" << endl;
 
                 Enter(zamestnanecNaLepeni, 1);
                 cout << "Dizajn info: Zabiram 1 zamestance na nalepeni designu" << endl;
 
                 Wait(Exponential(1500)); // lepeni designu
-                cout << "Dizajn info: Auto je dokoncene a pripravene na predaj zakaznikovy" << endl;
+                cout << "Dizajn info: vozidlo je dokoncene a pripravene na predaj zakaznikovy" << endl;
 
                 Leave(zamestnanecNaLepeni, 1);
                 cout << "Dizajn info: Uvolnuji 1 zamestance na nalepeni designu" << endl;
 
-                Leave(miestaPreAuta, 1);
-                cout << "Miesta pre auta: Uvolnujem 1 parkovacie miesto na dielni." << endl;   
+                Leave(miestaPrevozidla, 1);
+                cout << "Miesta pre vozidla: Uvolnujem 1 parkovacie miesto na dielni." << endl;   
             }
             // Polep standardni
             else if (pravdepodobnost > 40) // 60% pravdepodobnost
@@ -241,24 +247,24 @@ class Zakaznik : public Process
                 cout << "Lepenie info: Zabiram 1 zamestance na polep" << endl;
 
                 Seize(myciBox);
-                cout << "Lepenie info: Zabiram myci box pred polepem auta" << endl;
+                cout << "Lepenie info: Zabiram myci box pred polepem vozidla" << endl;
 
-                Wait(Exponential(120)); // myti auta
+                Wait(Exponential(120)); // myti vozidla
 
                 Release(myciBox);
                 cout << "Lepenie info: Uvolnuji myci box a jdu na polep" << endl;
 
-                Wait(Exponential(1200)); // polep auta
-                cout << "Lepenie info: Auto je dokoncene a pripravene na predaj zakaznikovy" << endl;
+                Wait(Exponential(1200)); // polep vozidla
+                cout << "Lepenie info: vozidlo je dokoncene a pripravene na predaj zakaznikovy" << endl;
 
                 Leave(zamestnanecNaLepeni, 1);
                 cout << "Lepenie info: Uvolnuji 1 zamestance na lepeni" << endl;
 
-                Leave(miestaPreAuta, 1);
-                cout << "Miesta pre auta: Uvolnujem 1 parkovacie miesto na dielni." << endl;
+                Leave(miestaPrevozidla, 1);
+                cout << "Miesta pre vozidla: Uvolnujem 1 parkovacie miesto na dielni." << endl;
             }
         }
-        // Nieje volne parkovacie miesto pre auto v dielni
+        // Nieje volne parkovacie miesto pre vozidlo v dielni
         else
         {
             cout << "Nieje volne parkovacie miesto na dielni." << endl;
@@ -270,8 +276,8 @@ class Zakaznik : public Process
 
                 Wait(Exponential(480));
 
-                // Miesto pre auto na dielni je volne
-                if (miestaPreAuta.Free() != 0)
+                // Miesto pre vozidlo na dielni je volne
+                if (miestaPrevozidla.Free() != 0)
                 {
                     cout << "Pri druhom prichode do dielne bolo volne parkovacie miesto. Zakaznik ide do obsluhy." << endl;
                     // goto?
@@ -314,7 +320,7 @@ int main(int argc, char * argv[])
     spracovanie_argumentov(argc, argv);
 
     // Vystupne data simulacie
-    SetOutput("polepy_out.dat");
+    SetOutput("data_out.dat");
     Print("Model firmy na polepy aut");
 
     cout << "*** Inicializacia ***" << endl;
@@ -330,11 +336,8 @@ int main(int argc, char * argv[])
     
     myciBox.Output();
     
-    miestaPreAuta.Output();
-    autoNaPozicanie.Output();
-
-    pozicanieAuta.Output();
-    dokonceniePoziadavku.Output();
+    miestaPrevozidla.Output();
+    vozidloNaPozicanie.Output();
 
     SIMLIB_statistics.Output();
 }
